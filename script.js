632290b1da460714d4bc483632760453135ecf64
KaiLee <125817626+KaiLee-Bot@users.noreply.github.com>
2024-10-11 19:37:13 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Adicione qualquer funcionalidade JavaScript aqui, se necessário
+document.getElementById('meuAudio').addEventListener('play', function() {
+    console.log('A música está tocando!');
+});
